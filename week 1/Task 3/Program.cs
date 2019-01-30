f7b69ed2f1f8d9c2c395afc5652f369bc14e9c1d
dokerbolov <proface19@gmail.com>
2019-01-30 23:09:13 +0600

    fourth commit

--- week 1/Task 3/Program.cs
+++ week 1/Task 3/Program.cs
@@ -1,10 +1,10 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace ConsoleApp7
 {
     class Program
     {
@@ -12,14 +12,17 @@ namespace ConsoleApp5
         {
             int n = int.Parse(Console.ReadLine());
             int[] a = new int[n];
-            for(int i = 0; i < n; i++)
+            for (int i=0;i<n;i++)
             {
                 int k = int.Parse(Console.ReadLine());
                 a[i] = k;
             }
-            for (int i = 0; i < n; i++)
+            for (int i=0; i<n; i++)
             {
-                Console.Write(a[i] + " ");
+                for (int j=0;j<2;j++)
+                {
+                    Console.Write(a[i] + " ");
+                }
             }
             Console.ReadKey();
         }
