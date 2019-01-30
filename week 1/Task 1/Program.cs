85ffff44c730aaa336c33cfab5ed83e832c3c378
dokerbolov <proface19@gmail.com>
2019-01-31 01:11:45 +0600

    fifth commit

--- week 1/Task 1/Program.cs
+++ week 1/Task 1/Program.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +10,36 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] a = new int[n];
-            for(int i = 0; i < n; i++)
-            {
-                int k = int.Parse(Console.ReadLine());
-                a[i] = k;
-            }
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(a[i] + " ");
-            }
+             int c = 0;
+             int n = int.Parse(Console.ReadLine());
+             int[] a = new int[n];
+             for (int i = 0; i < n; i++)
+             {
+                 a[i] = int.Parse(Console.ReadLine());
+             }
+             for (int i = 0; i < n; i++)
+                    {
+                        if (a[i] == 1)
+                        {
+                            c++;
+                        }
+                        for (int j = 2; j <= a[i]/2; j++)
+                        {
+                            if(a[i] % j == 0)
+                            {
+                                 c++;
+                                 break;
+                            }
+                        }
+                     if (c == 0)
+                     {
+                         Console.Write(a[i] + " ");
+                     }
+                     else
+                     {
+                         c = 0;
+                     }
+                   }
             Console.ReadKey();
         }
     }
