e18d96e7c18e248a7d789dbf571cc82c7859c27c
dokerbolov <proface19@gmail.com>
2019-02-01 15:39:30 +0600

    fifth commit

---
+++ week 1/Task 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public string year;
+        public Student(string name, string id, string year)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = year;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+            Console.WriteLine(year);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Yeldos", "18BD111111", "2019");
+            student.PrintInfo();
+
+        }
+    }
+}
--- week 1/Task 3/Program.cs
+++ week 1/Task 3/Program.cs
@@ -12,13 +12,11 @@ namespace ConsoleApp7
         {
             int n = int.Parse(Console.ReadLine());
             int[] a = new int[n];
-            for (int i=0;i<n;i++)
-            {
-                int k = int.Parse(Console.ReadLine());
-                a[i] = k;
-            }
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
             for (int i=0; i<n; i++)
             {
+                a[i] = int.Parse(ss[i]);
                 for (int j=0;j<2;j++)
                 {
                     Console.Write(a[i] + " ");
