fab82c92860be7deab1e24b424653f4fae62d093
dokerbolov <proface19@gmail.com>
2019-02-08 10:20:03 +0600

    fifth commit

--- week 2/Task 3/Task 3/Program.cs
+++ week 2/Task 3/Task 3/Program.cs
@@ -11,10 +11,33 @@ namespace Example4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\сетевая папка");
-            
+            int cnt = 0;
+            DirectoryInfo dir = new DirectoryInfo(@"C:\сетевая папка");
+            Address(dir, cnt);
+        }
+        public static void Address(DirectoryInfo dir1, int tab)
+        {
+            foreach (DirectoryInfo x in dir1.GetDirectories())
+            {
+                Space(tab);
+                Console.WriteLine(x.Name);
+                Address(x, tab + 1);
+            }
+            foreach (FileInfo y in dir1.GetFiles())
+            {
+                Space(tab);
+                Console.WriteLine(y.Name);
+            }
+
+        }
+        static void Space(int tab)
+        {
+            for(int i=0;i<tab;i++)
+            {
+                Console.Write("    ");
+            }
         }
     }
 }
---
+++ week 2/Task 4/Task 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = Console.ReadLine();
+            string sourcePath = @"C:\Users\profa\Desktop\KBTU\1-2\PP2\week 2\Task 4\input";
+            string targetPath = @"C:\Users\profa\Desktop\KBTU\1-2\PP2\week 2\Task 4\output";
+            string sourceFile = Path.Combine(sourcePath, filename);
+            string destFile = Path.Combine(targetPath, filename);
+            if(!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+            File.Create(sourcePath + @"\" + filename).Dispose();
+            Console.WriteLine("Press SPACE to copy the file");
+            ConsoleKeyInfo pressedKey = Console.ReadKey();
+            if (pressedKey.Key == ConsoleKey.Spacebar)
+            {
+                File.Copy(sourceFile, destFile);
+            }
+            Console.WriteLine("Press Backspace to copy the file");
+            ConsoleKeyInfo pressedKey2 = Console.ReadKey();
+            if (pressedKey2.Key == ConsoleKey.Backspace) 
+            {
+                File.Delete(sourcePath + @"\" + filename);
+            }
+
+        }
+    }
+}
